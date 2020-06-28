34d6edcf0edf151356ec460c6bc684b4bce757d5
Cobalt <vlavladvladik@mail.ru>
2020-06-28 18:12:03 +0300

    welcome fragment was added

--- app/src/main/java/ru/cobalt/telegram/clone/MainActivity.java
+++ app/src/main/java/ru/cobalt/telegram/clone/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.main_container, new WelcomeFragment())
+                .commit();
     }
 }
